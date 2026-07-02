e0ed44db1b04120d90977f09bf6291c4bbff8764
Mannat Verma <vmannat2007@gmail.com>
2026-07-02 23:48:45 +0530

    left roate one

---
+++ leftrotatearrayone.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void rotateArrayByOne(int[] nums) {
+        int n=nums.length;
+        int first=nums[0];
+        for(int i=0;i<n-1;i++){
+            nums[i]=nums[i+1];
+        }
+        nums[n-1]=first;
+    }
+}
