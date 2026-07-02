8c5ac1894b8f3863abb78b421ed04560c1379d9a
Palak <valtorin8@gmail.com>
2026-07-03 00:23:26 +0530

    Fix order of reverse calls in rotateArray method

--- array/easy/leftrotatebykplace.java
+++ array/easy/leftrotatebykplace.java
@@ -3,9 +3,9 @@ class Solution {
         int n = nums.length;
         k = k % n; // handle k > n
         
-        reverse(nums, 0, n - 1);      // reverse whole array
-        reverse(nums, 0, k - 1);      // reverse first k elements
+       reverse(nums, 0, k - 1);      // reverse first k elements
         reverse(nums, k, n - 1);      // reverse remaining elements
+        reverse(nums, 0, n - 1);      // reverse whole array
     }
     
     private void reverse(int[] nums, int start, int end) {
