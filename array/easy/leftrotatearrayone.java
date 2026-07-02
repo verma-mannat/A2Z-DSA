7d411489c0901e9c7039ea0264944df9761d5131
Mannat Verma <vmannat2007@gmail.com>
2026-07-02 23:59:16 +0530

    new changes

--- array/easy/leftrotatearrayone.java
+++ array/easy/leftrotatearrayone.java
@@ -1,3 +1,4 @@
+package array.easy;
 /*Given an integer array nums, rotate the array to the left by one.
 Note: There is no need to return anything, just modify the given array. */
 class Solution {
